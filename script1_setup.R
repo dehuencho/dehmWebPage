6e1ce93828abb27bc8b6b99678fae290c045e760
Daniel Huencho <dehuencho@uc.cl>
2021-01-21 16:00:40 -0300

    First commit

---
+++ script1_setup.R
@@ -0,0 +1,5 @@
+blogdown::new_site(theme = "wowchemy/starter-academic")
+
+blogdown::new_post(title = "First post rmarkdown", 
+                   ext = '.Rmarkdown', 
+                   subdir = "post")
