925f11837074b2583eefe398ff46fb217281bdd3
Daniel Huencho <dehuencho@uc.cl>
2021-01-22 22:45:49 -0300

    Second commit - serve site

--- script1_setup.R
+++ script1_setup.R
@@ -3,3 +3,5 @@ blogdown::new_site(theme = "wowchemy/starter-academic")
 blogdown::new_post(title = "First post rmarkdown", 
                    ext = '.Rmarkdown', 
                    subdir = "post")
+
+blogdown::serve_site()
